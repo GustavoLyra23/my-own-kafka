1266bad874a801404d9eea5521c1596e70acb1eb
GustavoLyra23 <gustavolyra23@gmail.com>
2025-06-08 16:51:43 -0300

    [fix Implementation of thread pool to handle multiple api calls v9]

--- src/main/java/core/models/ResponseDTO.java
+++ src/main/java/core/models/ResponseDTO.java
@@ -24,59 +24,52 @@ public class ResponseDTO {
 
     public ByteBuffer toByteBuffer() {
         try {
-            int bodySize = calculateBodySize();
-            LOGGER.fine("Creating response buffer with body size: " + bodySize);
-
-            var buff = ByteBuffer.allocate(4 + bodySize);
-
-            buff.putInt(bodySize);
-
-            buff.putInt(correlationId);
-            buff.putShort(errorCode);
-            buff.putShort((short) apiVersions.size());
-
-            for (ApiVersion apiVersion : apiVersions) {
-                buff.putShort((short) apiVersion.apiKey());
-                buff.putShort((short) apiVersion.minVersion());
-                buff.putShort((short) apiVersion.maxVersion());
-                buff.put((byte) 0);
-            }
-
-            buff.putInt(throttleTime);
-            buff.put((byte) 0);
-
+            var buff = createBuffer();
             LOGGER.fine("Response buffer created successfully, total size: " + buff.capacity());
             return buff;
 
         } catch (BufferOverflowException e) {
             LOGGER.log(Level.SEVERE, "Buffer overflow when creating response", e);
-
-            int safeSize = 1024;
-            var buff = ByteBuffer.allocate(safeSize);
-
-            int bodySize = calculateBodySize();
-            buff.putInt(bodySize);
-            buff.putInt(correlationId);
-            buff.putShort(errorCode);
-            buff.putShort((short) apiVersions.size());
-
-            for (ApiVersion apiVersion : apiVersions) {
-                buff.putShort((short) apiVersion.apiKey());
-                buff.putShort((short) apiVersion.minVersion());
-                buff.putShort((short) apiVersion.maxVersion());
-                buff.put((byte) 0);
-            }
-
-            buff.putInt(throttleTime);
-            buff.put((byte) 0);
-
-            return buff;
+            return createBuffer();
         }
     }
 
     private int calculateBodySize() {
-        int size = 4 + 2 + 2 + (apiVersions.size() * 7) + 4 + 1;
+        // Body size calculation:
+        // 4 bytes: correlation_id (response header)
+        // 2 bytes: error_code
+        // 1 byte: COMPACT_ARRAY length for api_keys
+        // (apiVersions.size() * 7) bytes: each ApiKey (2+2+2+1 bytes)
+        // 4 bytes: throttle_time_ms
+        // 1 byte: TAG_BUFFER for response
+        int size = 4 + 2 + 1 + (apiVersions.size() * 7) + 4 + 1;
         LOGGER.fine("Calculated body size: " + size);
         return size;
+    }
+
+    private ByteBuffer createBuffer() {
+        int bodySize = calculateBodySize();
+        LOGGER.fine("Creating response buffer with body size: " + bodySize);
+
+        var buff = ByteBuffer.allocate(4 + bodySize);
+
+        buff.putInt(bodySize);
+
+        buff.putInt(correlationId);
+
+        buff.putShort(errorCode);
+
+        buff.put((byte) (apiVersions.size() + 1));
+
+        for (ApiVersion apiVersion : apiVersions) {
+            buff.putShort((short) apiVersion.apiKey());
+            buff.putShort((short) apiVersion.minVersion());
+            buff.putShort((short) apiVersion.maxVersion());
+            buff.put((byte) 0);
+        }
+
+        buff.putInt(throttleTime);
+        buff.put((byte) 0);
+        return buff;
     }
 }
